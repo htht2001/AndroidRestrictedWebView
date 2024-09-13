c943bc7884215e5a1a7cfb278ea2929ee3e9a505
htht2001 <97393694+htht2001@users.noreply.github.com>
2024-09-13 14:28:45 +0300

    Update MainActivity.java

--- app/src/main/java/com/webview/myapplication/MainActivity.java
+++ app/src/main/java/com/webview/myapplication/MainActivity.java
@@ -1,121 +1,35 @@
 package com.webview.myapplication;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.DownloadManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.net.http.SslError;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.webkit.CookieManager;
-import android.webkit.SslErrorHandler;
-import android.webkit.URLUtil;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
-import android.webkit.WebChromeClient.CustomViewCallback;
 import android.webkit.WebResourceResponse;
-import android.webkit.WebSettings;
-import android.webkit.WebSettings.PluginState;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Objects;
-
-class AdBlockerUtil {
-    private final HashSet<String> adHosts = new HashSet<>();
-
-    private static AdBlockerUtil mInstance;
-
-    public static AdBlockerUtil getInstance() {
-        if (mInstance == null) {
-            mInstance = new AdBlockerUtil();
-        }
-        return mInstance;
-    }
-
-    public void initialize(Context context) {
-        try {
-            InputStream inputStream = context.getAssets().open("adblock.txt");
-            loadHostsFromInputStream(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public boolean isAd(String url) {
-        if (url == null) {
-            return false;
-        }
-
-        String requestHost = Uri.parse(url).getHost();
-        
-        if (requestHost == null) {
-            return false;
-        }
-
-        if (adHosts.contains(requestHost)) {
-            return true;
-        }
-        
-        String[] subDomains = requestHost.split("\\.");
-
-        for (int i = subDomains.length - 1; i >= 0; i--) {
-            String domain = String.join(".", Arrays.copyOfRange(subDomains, i, subDomains.length));
-
-            if (adHosts.contains(domain)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private void loadHostsFromInputStream(InputStream inputStream) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                adHosts.add(line);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
-
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
     private final int STORAGE_PERMISSION_CODE = 1;
-    private static final String[] ALLOWED_DOMAINS = BuildConfig.ALLOWED_DOMAINS.split(",");
-    private static final String STARTUP_URL = BuildConfig.STARTUP_URL;
-    private static final String VIEW_MODE = BuildConfig.VIEW_MODE;
-    private static final boolean BLOCK_MEDIA = BuildConfig.BLOCK_MEDIA;
-    private static final boolean BLOCK_ADS = BuildConfig.BLOCK_ADS;
-    private static final boolean NO_SSL = BuildConfig.NO_SSL;
+    private static final String STARTUP_URL = "https://www.example.com";  // Replace with a valid URL
     private WebView mWebView;
-    private View mCustomView;
-    private CustomViewCallback mCustomViewCallback;
     private ProgressBar mProgressBar;
+    private ValueCallback<Uri> mUploadMessage;
+    private ValueCallback<Uri[]> mFilePathCallback;
+    private final static int FILECHOOSER_RESULTCODE = 1;
 
     private void requestStoragePermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
@@ -135,7 +49,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == FILECHOOSER_RESULTCODE) {
-            if (null == mUploadMessage && null == mFilePathCallback) return;
             Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
             if (mFilePathCallback != null) {
                 mFilePathCallback.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
@@ -147,108 +60,34 @@ public class MainActivity extends Activity {
         }
     }
 
-    private ValueCallback<Uri> mUploadMessage;
-    private ValueCallback<Uri[]> mFilePathCallback;
-    private final static int FILECHOOSER_RESULTCODE = 1;
-
-    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
-    @SuppressLint("SetJavaScriptEnabled")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AdBlockerUtil adBlockerUtil = AdBlockerUtil.getInstance();
-        adBlockerUtil.initialize(this);
-        if (VIEW_MODE.equals("PORTRAIT")) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        } else if (VIEW_MODE.equals("LANDSCAPE")) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        } else {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-        }
-
+        
         setContentView(R.layout.activity_main);
         mWebView = findViewById(R.id.activity_main_webview);
         mProgressBar = findViewById(R.id.progressBar);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setBuiltInZoomControls(true);
-        webSettings.setDisplayZoomControls(false);
-        webSettings.setSupportZoom(true);
-        webSettings.setDefaultTextEncodingName("utf-8");
-        webSettings.setPluginState(PluginState.ON);
-        webSettings.setAllowFileAccess(false);
-        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView, true);
-        if (BLOCK_MEDIA) {
-            webSettings.setLoadsImagesAutomatically(false);
-        }
-        webSettings.setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36");
 
+        if (mWebView == null) {
+            Log.e(TAG, "WebView not found in layout.");
+            return;
+        }
+        
         mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
-            public void onShowCustomView(View view, CustomViewCallback callback) {
-                if (mCustomView != null) {
-                    callback.onCustomViewHidden();
-                    return;
-                }
-                mCustomView = view;
-                mWebView.setVisibility(View.GONE);
-                mCustomViewCallback = callback;
-                ((FrameLayout) getWindow().getDecorView()).addView(mCustomView);
-                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-            }
-
-            @Override
-            public void onHideCustomView() {
-                ((FrameLayout) getWindow().getDecorView()).removeView(mCustomView);
-                mCustomView = null;
-                mWebView.setVisibility(View.VISIBLE);
-                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-                mCustomViewCallback.onCustomViewHidden();
-                mCustomViewCallback = null;
-                if (VIEW_MODE.equals("PORTRAIT")) {
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-                } else if (VIEW_MODE.equals("LANDSCAPE")) {
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-                } else {
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-                }
+            public void onShowCustomView(View view, WebChromeClient.CustomViewCallback callback) {
+                // Handle full-screen video playback if needed
             }
 
             @Override
             public boolean onShowFileChooser(
                 WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
                 mFilePathCallback = filePathCallback;
-
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-                if (fileChooserParams.getAcceptTypes().length > 0 && !Objects.equals(fileChooserParams.getAcceptTypes()[0], "")) {
-                    intent.setType(fileChooserParams.getAcceptTypes()[0]);
-                } else {
-                    intent.setType("*/*");
-                }
-
+                intent.setType("*/*");
                 startActivityForResult(intent, FILECHOOSER_RESULTCODE);
                 return true;
-            }
-
-            // support android < 5.0
-            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
-                mUploadMessage = uploadMsg;
-                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-                i.addCategory(Intent.CATEGORY_OPENABLE);
-                
-                if (acceptType != null && !acceptType.isEmpty()) {
-                    i.setType(acceptType);
-                } else {
-                    i.setType("*/*");
-                }
-                startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
             }
         });
 
@@ -260,23 +99,8 @@ public class MainActivity extends Activity {
             }
 
             @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                if (NO_SSL && (Objects.equals(error.getCertificate().getIssuedTo().getCName(), "NetFree") ||
-                        Objects.equals(error.getCertificate().getIssuedTo().getCName(), "Netspark"))) {
-                    handler.proceed();
-                } else {
-                    handler.cancel();
-                    view.loadData("<html><body><h1 style='color: grey'>SSL Error</h1></body></html>", "text/html; charset=utf-8", "UTF-8");
-                }
-            }
-
-            @Override
             public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
-                if (BLOCK_ADS && AdBlockerUtil.getInstance().isAd(url)) {
-                    return new WebResourceResponse("text/plain", "utf-8", new ByteArrayInputStream("".getBytes()));
-                } else {
-                    return super.shouldInterceptRequest(view, url);
-                }
+                return super.shouldInterceptRequest(view, url);
             }
         });
 
@@ -288,10 +112,11 @@ public class MainActivity extends Activity {
     }
 
     private void startWebView() {
-        if (URLUtil.isValidUrl(STARTUP_URL)) {
+        try {
             mWebView.loadUrl(STARTUP_URL);
-        } else {
-            Toast.makeText(this, "Invalid URL", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e(TAG, "Error loading URL: " + e.getMessage());
+            Toast.makeText(this, "Error loading URL", Toast.LENGTH_SHORT).show();
         }
     }
 }
