e9578df1c402e06ea9cce5d28834a98f29f65472
htht2001 <97393694+htht2001@users.noreply.github.com>
2024-09-13 15:53:49 +0300

    Update MainActivity.java

--- app/src/main/java/com/webview/myapplication/MainActivity.java
+++ app/src/main/java/com/webview/myapplication/MainActivity.java
@@ -116,7 +116,8 @@ public class MainActivity extends Activity {
     private View mCustomView;
     private CustomViewCallback mCustomViewCallback;
     private ProgressBar mProgressBar;
-
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36";
+    
     private void requestStoragePermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
             new AlertDialog.Builder(this)
